667e8f68c977c591c5b750815890aff626dd93d0
Ineiles Reyes <ineiles.reyes@prestohoy.com.ar>
2021-05-15 00:36:31 -0300

    commit number 3

--- src/main/kotlin/LogicOperators.kt
+++ src/main/kotlin/LogicOperators.kt
@@ -6,5 +6,20 @@ fun main () {
     var segundoImporte = 3
 
     println ("la suma de de $primerImporte + $segundoImporte es igual a: ${primerImporte+segundoImporte}")
-println("¿está esto bien guardado?")
+
+    // algoritmo secuencial de cálculo y promedio:
+
+    var num1 = 9.0
+    var num2 = 3.0
+    var num3 = 2.0
+    var promedio = (num1+num2+num3)/num3
+    println("el promedio de \nnum1= $num1 \nnum2= $num2 \nnum3= $num3 \nes= $promedio")
+
+    // operadores unarios:
+    var cosa = 1
+    cosa++
+    println("el valor de cosa incrementado es: $cosa")
+
+    cosa--
+    println("el valor de cosa decrementado es: $cosa")
 }
